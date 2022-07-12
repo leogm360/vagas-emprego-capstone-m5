2138fa0d7b6c2926a66576fd808c663a39b176ff
Rodrigo <rfirmos@gmail.com>
2022-07-12 10:32:39 -0300

    :sparkles: feat: create model and serializer

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Address(models.model):
+    zip_code = models.CharField(max_length=8)
+    street = models.CharField(max_length=255)
+    number = models.IntegerField()
+    complement = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    country = models.CharField(max_length=20)
