77f7bddd3cbc9c8f500e625b7ea24e0b10f64256
paulohliima <paulo.lima-16@hotmail.com>
2022-07-12 10:28:19 -0300

    :sparkles: feat: create serializer and add models field

--- jobs/models.py
+++ jobs/models.py
@@ -1,27 +1,40 @@
-from tkinter import CASCADE
 from django.db import models
+from django.core.validators import MinValueValidator
+
+class JobChoices(models.TextChoices):
+    CLT = ("CLT",)
+    PJ = ("PJ",)
+    TEMPORARY = ("TEMPORARY",)
+
+class RegimeChoices(models.TextChoices):
+    PRESENCIAL = ("PRESENCIAL",)
+    REMOTE = ("REMOTE",)
+    HYBRID = ("HYBRID",)
+
 
 # Create your models here.
 class Job(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     salary = models.DecimalField()
-    location = models.TextChoices()
-    job_type = models.CharField(max_length=20)
-    regimen_type = models.CharField(max_length=20)
-    vacancies_count = models.IntegerField()
-    subscribers_count = models.IntegerField()
+    location = models.CharField(max_length=50)
+    job_type = models.CharField(max_length=9, choices=JobChoices.choices,
+        default=JobChoices.DEFAULT)
+    regimen_type = models.CharField(max_length=10, choices=RegimeChoices.choices,
+        default=RegimeChoices.DEFAULT)
+    vacancies_count = models.IntegerField(validators=[MinValueValidator(1)])
+    subscribers_count = models.IntegerField(default=0)
     issued_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField()
+    is_active = models.BooleanField(default=True)
 
     account = models.ManyToManyField(
-        "accounts.Account", related_name="job_account"
+        "accounts.Account", related_name="accounts_id"
     )
 
     skill = models.ManyToManyField(
-        "skills.Skill", related_name="job_skill"
+        "skills.Skill", related_name="skill_id"
     )
 
     company = models.ForeignKey(
-        "companies.Company", on_delete=CASCADE, related_name="job_company_id"
+        "companies.Company", on_delete=models.CASCADE, related_name="company_id"
     )
