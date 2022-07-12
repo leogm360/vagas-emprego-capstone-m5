d52818810e8d7bd1445ef54e73cafeffa941e7a5
Bernardo Enock <bernardoenockgm@gmail.com>
2022-07-12 14:11:19 -0300

    :rocket: request-collections

--- README.md
+++
@@ -1,2 +0,0 @@
-# vagas-emprego-capstone-m5
-API backend que simula a interação de um usuário com empresas e vagas de emprego.
--- addresses/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from addresses.models import Address
-
-
-class AddressSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Address
-        fields = "__all__"
