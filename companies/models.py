ca14562f2d1a6c1bddd6113d65b6d8d6f2c833b1
Marcio Jose Tonholo Simoes Junior <you@example.com>
2022-07-12 12:18:30 -0300

    :sparkles: feat:implement companies models test, views, models, serializers

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
-# Create your models here.
+class Company(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name=models.CharField(max_length=255)
+    cnpj=models.CharField(max_length=14) 
+    phone=models.CharField(max_length=11)
+    date_joined= models.DateTimeField(default=timezone.now())
+    adress= models.OneToOneField("addresses.Adress",on_delete=models.CASCADE)
--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Company 
+from .serializers import CompanySerializer
+from django.shortcuts import get_object_or_404
+from addresses.models import Adress
 
-# Create your views here.
+class CompanyView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+    def perform_create(self,serializer):
+        adress = get_object_or_404(Adress,zip_code = self.request.zip_code )
+        serializer.save(adress=adress)  
+
+
