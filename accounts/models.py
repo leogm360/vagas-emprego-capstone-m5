f6d5236283953944c9e8006813d047dfc02f8cd6
Bernardo Enock <bernardoenockgm@gmail.com>
2022-07-12 11:17:56 -0300

    :sparkles: feat: accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+from .utils import CustomUserManager
+
+
+class GenderFieldChoice(models.TextChoices):
+    cisgender = ("Cisgender", "C")
+    transgender = ("Transgender", "T")
+    non_binary = ("Non Binary", "NB")
+
+class Account(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, unique=True)
+    gender = models.CharField(
+        max_length=50,
+        choices=GenderFieldChoice.choices,
+    )
+    phone = models.CharField(max_length=11, unique=True)
+
+    is_human_resources = models.BooleanField(default=False)
+    # address = models.OneToOneField(
+    #     "addresses.Address", on_delete=models.CASCADE, null=True
+    # )
+
+    # company = models.ForeignKey(
+    # "companies.Company", on_delete=models.CASCADE, related_name="accounts", null=True
+    # )
+
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name', 
+        'last_name', 
+        'cpf', 
+        'gender', 
+        'phone',
+        ]
+    objects = CustomUserManager()
