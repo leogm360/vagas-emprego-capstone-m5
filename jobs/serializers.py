089e97574724407c4e768b8e6f0bbe7b41811785
paulohliima <paulo.lima-16@hotmail.com>
2022-07-13 09:16:33 -0300

    :sparkles: feat: create structure serializer

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.serializers import SerializerMethodField
 from jobs.models import Job
 
 class JobSerializer(serializers.ModelSerializer):
-    location = SerializerMethodField(read_only=True)
+    # location = SerializerMethodField(read_only=True)
 
     class Meta:
         model = Job
@@ -12,4 +12,4 @@ class JobSerializer(serializers.ModelSerializer):
         read_only_fields = ['id','subscribers_count','issued_at','is_active','company_id']
 
     # def get_location(self, address: Address) -> str:
-    #     return address
+    #     return Company.address.city
