64e3cec9c98c2dd03bd0948f4af523d16647eec7
Andre <heraandre@gmail.com>
2022-07-14 00:19:18 -0300

    feat: starting job views

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from jobs.models import Job
+from jobs.serializers import JobSerializer
+
+class JobView(generics.ListCreateAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class DetailJobView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
