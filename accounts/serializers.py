5d3b63d7985d50da5c2115a4cb2ee02b42bccc56
paulohliima <paulo.lima-16@hotmail.com>
2022-07-14 15:31:19 -0300

    :bug: fix: refactor model account and serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -35,8 +35,19 @@ class AccountSerializer(serializers.ModelSerializer):
 class ListAccountsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = "__all__"
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "cpf",
+            "gender",
+            "phone",
+            "address",
+            "is_human_resources",
+            "is_superuser",
+            "company_id"
+            ]
 
 
 class LoginSerializer(serializers.Serializer):
