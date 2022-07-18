7914c1ad899b9722847717da7bd91088e826e32b
leogm360 <leogm360@gmail.com>
2022-07-18 12:21:48 -0300

    :bug: fix: humam resources accounts
      - fix id not beign returned when registering a new human resources
        account.

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -4,5 +4,17 @@ class SerializerByMethodMixin:
         #     return DetailParkingLotSerializer
         # elif self.request.method == 'GET':
         #     return ListParkingLotSerializer
-        return self.serializer_map.get(self.request.method, self.serializer_class)
+        return self.serializer_map.get(
+            self.request.method, self.serializer_class
+        )
         # return self.serializer_map.get("POST")
+
+
+class SerializerByAccountTypeMixin:
+    def get_serializer_class(self, *args, **kwargs):
+        is_humam_resources = self.request.data.get("is_human_resources", False)
+
+        if is_humam_resources:
+            return self.serializer_map.get("HUMAN_RESOURCES")
+        else:
+            return self.serializer_map.get("CANDIDATE")
