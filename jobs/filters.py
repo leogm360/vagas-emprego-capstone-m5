f36194d6d0f2d07a11015c014c8981e2cb47c7dc
Rodrigo <rfirmos@gmail.com>
2022-07-18 14:58:37 -0300

    :sparkles: feat : add filters to jobs

--- jobs/filters.py
+++ jobs/filters.py
@@ -1,48 +1,50 @@
 from django.db.models import Q
 
-from django_filters.rest_framework import FilterSet, CharFilter, NumberFilter, DateTimeFilter
+from django_filters.rest_framework import (
+    FilterSet,
+    CharFilter,
+    NumberFilter,
+    DateTimeFilter,
+)
 
 from .models import Job
 
+
 class JobsFilter(FilterSet):
     # GET /api/jobs/search?q=’desenvolvedor fullstack junior’
-    q = CharFilter(
-        method="filter_by_text", label="Search By Text", required=True
-    )
+    q = CharFilter(method="filter_by_text", label="Search By Text")
     # GET /api/jobs/search?issued=oldest
-    issued = DateTimeFilter(field_name='issued_at', lookup_expr='gte')
+    issued = DateTimeFilter(field_name="issued_at", lookup_expr="gte")
     # GET /api/jobs/search?type=presential | hybrid | remote
-    type = CharFilter(field_name='job_type', lookup_expr='iexact')
+    type = CharFilter(field_name="job_type", lookup_expr="iexact")
     # GET /api/jobs/search?vacancies=1
-    vacancies = NumberFilter(field_name='vacancies_count', lookup_expr='lte')
+    vacancies = NumberFilter(field_name="vacancies_count", lookup_expr="lte")
     # GET /api/jobs/search?subscribers=1
-    subscribes = NumberFilter(field_name='account__id', lookup_expr='icontains')
+    subscribes = NumberFilter(field_name="account__id", lookup_expr="icontains")
 
     """
     GET /api/jobs/search?salary=3567.29
     GET /api/jobs/search?location=RJ
     GET /api/jobs/search?skills=Django
     """
+
     class Meta:
         model = Job
-        fields = [ 
-            "issed_at", 
-            "job_type", 
-            "vacancies_count", 
-            "account", 
-            "salary", 
-            "location", 
-            "skills"
-            ]
+        fields = [
+            "issued_at",
+            "job_type",
+            "vacancies_count",
+            "account",
+            "salary",
+            "location",
+            "skills",
+        ]
 
     def filter_by_text(self, queryset, name, value):
         return queryset.filter(
-            Q(title__icontains=value) | 
-            Q(description__icontains=value) |
-            Q(location__icontains=value) |
-            Q(job_type__icontains=value) |
-            Q(regimen_type__icontains=value)
+            Q(title__icontains=value)
+            | Q(description__icontains=value)
+            | Q(location__icontains=value)
+            | Q(job_type__icontains=value)
+            | Q(regimen_type__icontains=value)
         )
-
-
-
--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import generics
 
 from .filters import JobsFilter
@@ -9,8 +10,10 @@ from .serializers import JobSerializer
 
 # from .permissions
 
+
 class SearchJobsView(generics.ListAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    filter_backends = (DjangoFilterBackend)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = JobsFilter
+    ordering_fields = "__all__"
